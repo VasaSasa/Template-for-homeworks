0b8d45fc2f7962b3d9e60e5768bc2f082bc3b8d9
Václav Doležal <bc.va.do@gmail.com>
2022-03-10 12:53:50 +0100

    created 3 files, template for homeworks

--- script.js
+++ script.js
@@ -1,1 +1,41 @@
 // alert("Ahojky")
+
+/*let user = Number(prompt("Zadej celé číslo.(kladné, záporné,nulu)"));
+if (user == 0) {
+    console.log("Zadal jsi nulu.");
+} else if (user > 0) {
+    console.log("Zadal jsi kladné číslo.");
+} else {
+    console.log("Zadal jsi záporné číslo.");
+}*/
+
+/*let user = Number(prompt("Zadejte svůj věk."));
+if (user > 0 && user <= 120) {
+    console.log("Váš věk je:",user,"rok/roky/roků.");
+} else {
+    console.log("Zadaný věk neodpovídá lidskému životu!!!");
+}*/
+
+
+
+/*let user = Number(prompt("Zadejte celé číslo.(V matematice označeno Z)"));
+if (user >= 0) {
+    console.log("|",user,"|","=",user,",","|-",user,"| =",user);
+} else {
+    console.log("Vaše číslo je záporné")
+}*/
+
+
+
+let user = Number(prompt("Zadejte celé číslo.(V matematice označeno Z)"));
+if (user >= 0) {
+    console.log("(|",user,"|=",user,", |-",user,"|=",user,")");
+} else {
+    console.log("(|",Math.abs(user),"|=",Math.abs(user),", |",user,"|=",Math.abs(user),")");
+}
+
+
+// let date = new Date();
+// console.log(date);
+// let hours = getHours();
+// console.log(hours);
